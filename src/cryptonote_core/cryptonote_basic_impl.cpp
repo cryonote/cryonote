87284af723e742e2552a937e03dcdf3a48f940c0
anythingtechpro <anythingtechpro@gmail.com>
2018-10-04 18:43:53 -0400

    cryptonote_core: fix for the 32-bit saturation bug...

--- src/cryptonote_core/cryptonote_basic_impl.cpp
+++ src/cryptonote_core/cryptonote_basic_impl.cpp
@@ -50,79 +50,90 @@ namespace cryptonote {
     size_t num_warmup_periods = sizeof(warmup_period_rewards) / sizeof(uint64_t) - 1;
     uint64_t total_warmup_period() { return config::week_height() * 3; }
     uint64_t warmup_period_length() { return total_warmup_period() / num_warmup_periods; }
-    
+
     // linear gradual cooldown over years
     size_t num_reward_eras = 4;
     uint64_t reward_era_length() { return 3 * config::year_height(); }
     uint64_t penalty_per_era = standard_reward / (num_reward_eras - 1);
   }
-  
+
   uint64_t get_base_reward(uint64_t target_block_height)
   {
     using namespace detail;
-    
+
     if (target_block_height == 0) // genesis block
+    {
       return 0;
-    
+    }
+
     if (config::in_pos_era(target_block_height)) // no more rewards once switch to DPOS
     {
       return 0;
     }
-    
+
     if (config::no_reward_ramp) // true for testing
     {
       return standard_reward;
     }
-    
+
     if (target_block_height < total_warmup_period())
     {
       size_t subperiod = target_block_height / warmup_period_length();
       uint64_t height_in_period = target_block_height % warmup_period_length();
       uint64_t start_reward = warmup_period_rewards[subperiod];
       uint64_t end_reward = warmup_period_rewards[subperiod + 1];
-      
+
       return start_reward + ((end_reward - start_reward) * height_in_period) / warmup_period_length();
     }
-    
+
     size_t reward_era = target_block_height / reward_era_length();
     if (reward_era >= num_reward_eras)
+    {
       return 0;
-    
+    }
+
     return standard_reward - penalty_per_era * reward_era;
   }
-  
+
   bool get_block_reward(size_t median_size, size_t current_block_size, uint64_t already_generated_coins, uint64_t target_block_height, uint64_t &reward) {
     uint64_t base_reward = get_base_reward(target_block_height);
     LOG_PRINT_L4("Base reward for height " << target_block_height << " is " << base_reward);
-    
-    //make it soft
-    if (median_size < CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE) {
+
+    // make it soft
+    if (median_size < CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE)
+    {
       median_size = CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE;
     }
 
-    if (current_block_size <= median_size) {
+    if (current_block_size <= median_size)
+    {
       reward = base_reward;
       return true;
     }
 
-    if(current_block_size > 2 * median_size) {
+    if(current_block_size > 2 * median_size)
+    {
       LOG_PRINT_L4("Block cumulative size is too big: " << current_block_size << ", expected less than " << 2 * median_size);
       return false;
     }
-    
+
     CHECK_AND_ASSERT(median_size < std::numeric_limits<uint32_t>::max(), false);
     CHECK_AND_ASSERT(current_block_size < std::numeric_limits<uint32_t>::max(), false);
-    
+
     uint64_t product_hi;
-    uint64_t product_lo = mul128(base_reward, current_block_size * (2 * median_size - current_block_size), &product_hi);
-    
+    // BUGFIX: 32-bit saturation bug (e.g. ARM7), the result was being
+    // treated as 32-bit by default.
+    uint64_t multiplicand = 2 * median_size - current_block_size;
+    multiplicand *= current_block_size;
+    uint64_t product_lo = mul128(base_reward, multiplicand, &product_hi);
+
     uint64_t reward_hi;
     uint64_t reward_lo;
     div128_32(product_hi, product_lo, static_cast<uint32_t>(median_size), &reward_hi, &reward_lo);
     div128_32(reward_hi, reward_lo, static_cast<uint32_t>(median_size), &reward_hi, &reward_lo);
     CHECK_AND_ASSERT(0 == reward_hi, false);
     CHECK_AND_ASSERT(reward_lo < base_reward || base_reward == 0, false);
-    
+
     reward = reward_lo;
     return true;
   }
@@ -132,7 +143,9 @@ namespace cryptonote {
     const unsigned char* pbuf = reinterpret_cast<const unsigned char*>(&bl);
     uint8_t summ = 0;
     for(size_t i = 0; i!= sizeof(public_address_outer_blob)-1; i++)
+    {
       summ += pbuf[i];
+    }
 
     return summ;
   }
@@ -145,10 +158,14 @@ namespace cryptonote {
   bool is_coinbase(const transaction& tx)
   {
     if(tx.ins().size() != 1)
+    {
       return false;
+    }
 
     if(tx.ins()[0].type() != typeid(txin_gen))
+    {
       return false;
+    }
 
     return true;
   }
@@ -249,7 +266,7 @@ namespace cryptonote {
              a.dpos_sig == b.dpos_sig;
     }
   }*/
-  
+
   // rough & ready easy equality for sanity checking only
   bool operator ==(const cryptonote::transaction& a, const cryptonote::transaction& b) {
     return cryptonote::get_transaction_hash(a) == cryptonote::get_transaction_hash(b);
@@ -258,11 +275,11 @@ namespace cryptonote {
   bool operator ==(const cryptonote::block& a, const cryptonote::block& b) {
     return cryptonote::get_block_hash(a) == cryptonote::get_block_hash(b);
   }
-  
+
   bool operator !=(const cryptonote::transaction& a, const cryptonote::transaction& b) {
     return !(a == b);
   }
-  
+
   bool operator !=(const cryptonote::block& a, const cryptonote::block& b) {
     return !(a == b);
   }
