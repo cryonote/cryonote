b21c91dcf02bd3f7a9af4213db12049f5578326a
anythingtechpro <anythingtechpro@gmail.com>
2018-10-04 22:15:51 -0400

    rpc: fixed typo on_getblockhash -> getblockhash

--- src/rpc/core_rpc_server.h
+++ src/rpc/core_rpc_server.h
@@ -55,7 +55,7 @@ namespace cryptonote
       MAP_URI_AUTO_JON2("/getautovotedelegates", on_get_autovote_delegates, COMMAND_RPC_GET_AUTOVOTE_DELEGATES)
       BEGIN_JSON_RPC_MAP("/json_rpc")
         MAP_JON_RPC("getblockcount",             on_getblockcount,              COMMAND_RPC_GETBLOCKCOUNT)
-        MAP_JON_RPC_WE("on_getblockhash",        on_getblockhash,               COMMAND_RPC_GETBLOCKHASH)
+        MAP_JON_RPC_WE("getblockhash",        on_getblockhash,               COMMAND_RPC_GETBLOCKHASH)
         MAP_JON_RPC_WE("getblocktemplate",       on_getblocktemplate,           COMMAND_RPC_GETBLOCKTEMPLATE)
         MAP_JON_RPC_WE("submitblock",            on_submitblock,                COMMAND_RPC_SUBMITBLOCK)
         MAP_JON_RPC_WE("getlastblockheader",     on_get_last_block_header,      COMMAND_RPC_GET_LAST_BLOCK_HEADER)
