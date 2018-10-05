47d6a30f988ddb4bd56c5b1dbfdad2b6b08e40c9
anythingtechpro <anythingtechpro@gmail.com>
2018-10-05 19:08:57 -0400

    added new line ending for print_version message

--- src/print_version.cpp
+++ src/print_version.cpp
@@ -8,7 +8,7 @@ int main(int argc, char **argv)
   std::cout << PROJECT_VERSION_MAJOR << "."
             << PROJECT_VERSION_MINOR << "."
             << PROJECT_VERSION_REVISION << "."
-            << PROJECT_VERSION_BUILD;
-  
+            << PROJECT_VERSION_BUILD << std::endl;
+
   return 0;
 }
