0b66e5936e79f55d4aea1319defb0058fde147db
anythingtechpro <anythingtechpro@gmail.com>
2018-10-17 21:33:09 -0400

    build_system: update reference to coin name

--- build_system/linux-x64.sh
+++ build_system/linux-x64.sh
@@ -37,7 +37,7 @@ VERSION=`src/print_version`
 REPO_BRANCH=`git branch -r --contains HEAD | tail -n 1`
 REPO=`echo $REPO_BRANCH | cut -d/ -f1`
 BRANCH=`echo $REPO_BRANCH | cut -d/ -f2`
-ARCHIVE=pebblecoin-all-$PLATFORM_NAME-v$VERSION-$REPO-$BRANCH-$BUILD_NUMBER.tar.gz
+ARCHIVE=cryonote-all-$PLATFORM_NAME-v$VERSION-$REPO-$BRANCH-$BUILD_NUMBER.tar.gz
 
 cd src
 # strip
