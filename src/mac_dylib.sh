1afa8567411e4e6fc0ce38cef82268002d337acc
anythingtechpro <anythingtechpro@gmail.com>
2018-10-24 16:16:10 -0400

    update references to coin name

--- src/mac_dylib.sh
+++ src/mac_dylib.sh
@@ -2,7 +2,7 @@
 
 echo Packaging dynamic libraries...
 # list all the local libs
-DYLIBS=`otool -L pebblecoin-qt | grep /usr/local | grep -v :$ | awk -F' ' '{ print $1 }'`
+DYLIBS=`otool -L cryonote-qt | grep /usr/local | grep -v :$ | awk -F' ' '{ print $1 }'`
 # go recursively to handle symlinks
 for dylib in $DYLIBS; do
 DYLIBS="$DYLIBS `otool -L $dylib | grep /usr/local | grep -v :$ | awk -F' ' '{ print $1 }'`";
@@ -14,10 +14,10 @@ for dylib in $DYLIBS; do
 cp $dylib lib; chmod 644 lib/`basename $dylib`;
 done;
 
-# replace pebblecoin-qt links and library links
+# replace cryonote-qt links and library links
 for dylib in $DYLIBS; do
 echo Rewriting `basename $dylib`...
-install_name_tool -change $dylib @executable_path/lib/`basename $dylib` pebblecoin-qt;
+install_name_tool -change $dylib @executable_path/lib/`basename $dylib` cryonote-qt;
 # change it in all the other libraries
 for dylib2 in $DYLIBS; do
 install_name_tool -change $dylib @executable_path/lib/`basename $dylib` lib/`basename $dylib2`;
